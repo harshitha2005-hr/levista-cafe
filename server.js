b61c4e62f63c272f7a85a9bb06b82a651c48dde4
harshitha2005-hr <harshitha2005.005@gmail.com>
2026-07-30 22:22:42 +0530

    Add files via upload

--- server.js
+++ server.js
@@ -1,31 +1,66 @@
 const express = require("express");
-const cors = require("cors"); // ✅ ADD THIS
+const cors = require("cors");
 
 const app = express();
 
-app.use(cors()); // ✅ ADD THIS
+app.use(cors());
 app.use(express.json());
 
 let orders = [];
 
+
 // PLACE ORDER
 app.post("/place-order", (req, res) => {
-    const order = req.body;
 
-    console.log("New Order Received:", order);
+    const order = {
+        id: Date.now(),
+        username: req.body.username,
+        tableNo: req.body.tableNo,
+        items: req.body.items,
+        status: "Pending",
+        time: new Date()
+    };
+
+    console.log("New Order:", order);
 
     orders.push(order);
 
     res.json({
-        message: "Order received successfully ✅"
+        success: true,
+        message: "Order placed successfully"
     });
 });
 
-// GET ORDERS
-app.get("/orders", (req, res) => {
+
+// GET ALL ORDERS FOR KITCHEN
+app.get("/orders", (req,res)=>{
     res.json(orders);
 });
 
-app.listen(5000, () => {
+
+// UPDATE ORDER STATUS
+app.put("/update-order/:id",(req,res)=>{
+
+    const id = Number(req.params.id);
+
+    const order = orders.find(o=>o.id===id);
+
+    if(order){
+        order.status=req.body.status;
+
+        res.json({
+            message:"Status updated"
+        });
+    }
+    else{
+        res.json({
+            message:"Order not found"
+        });
+    }
+
+});
+
+
+app.listen(5000,()=>{
     console.log("Server running on port 5000 🚀");
 });
